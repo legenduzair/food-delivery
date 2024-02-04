ab8b37b8094bf9815f0a210f78be0c0a6f1f0dda
Uzair Oomer <uzair.oomer@jdplc.com>
2024-02-04 13:54:53 +0000

    add navigation stack

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text className="text-red-500">HomeScreen</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
