0d4e19467262626e2ba66ce08d11a9322755c64a
Uzair Oomer <uzair.oomer@jdplc.com>
2024-02-13 13:22:26 +0000

    install sanity client and add code to connect to front end

---
+++ sanity.js
@@ -0,0 +1,14 @@
+import {createClient} from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = createClient({
+    projectId: 'c92p951s',
+    dataset: "production",
+    useCdn: true,
+    apiVersion: '2024-02-13',
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+
+export default client;
