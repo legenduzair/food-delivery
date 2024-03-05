8af1cbf6bcab722c4a94564ab67c86b4aa7eb6e2
Uzair Oomer <uzair.oomer@jdplc.com>
2024-03-05 16:49:21 +0000

    Add basket screen jsx

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { selectRestaurant } from '../features/restaurantSlice';
 import { selectBasketItems } from '../features/basketSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
+import { XCircleIcon } from "react-native-heroicons/solid";
 
 const BasketScreen = () => { 
   const navigation = useNavigation();
@@ -22,9 +23,32 @@ const BasketScreen = () => {
   }, [items]);
   
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 bg-gray-100">
+        <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">{restaurant.title}</Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={navigation.goBack}
+            className="rounded-full bg-gray-100 absolute top-3 right-5">
+            <XCircleIcon color="#00CCBB" height={50} width={50} />
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+          <Image source={{
+            url: "https://links.papareact.com/wru"
+          }} className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
+          <Text className="flex-1">Delivery in 40-60 mins</Text>
+          <TouchableOpacity>
+            <Text className="text-[#00CCBB]">Change</Text>
+          </TouchableOpacity>
+        </View>       
+      </View>
+    </SafeAreaView>
   )
 }
 
