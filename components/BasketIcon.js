f117ca8d332f86dd2dd19244dd8b5835065639cd
Uzair Oomer <uzair.oomer@jdplc.com>
2024-03-05 16:01:31 +0000

    add basket screen component, and restaurant slice to store restuarant details in global state

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -11,7 +11,7 @@ const BasketIcon = () => {
 
   return (
     <View className="absolute bottom-10 w-full z-50">
-      <TouchableOpacity className="bg-[#00CCBB] mx-5 p-4 rounded-lg flex-row
+      <TouchableOpacity onPress={() => navigation.navigate("Basket")} className="bg-[#00CCBB] mx-5 p-4 rounded-lg flex-row
       items-center space-x-1">
         <Text className="text-white font-extrabold text-lg
         bg-[#01A296] py-1 px-2">{items.length}</Text>
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,14 +1,16 @@
 import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { urlFor } from '../sanity';
 import { ArrowLeftIcon, ChevronRightIcon, QuestionMarkCircleIcon, StarIcon } from 'react-native-heroicons/solid';
 import { MapPinIcon } from 'react-native-heroicons/solid';
 import DishRow from '../components/DishRow';
 import BasketIcon from '../components/BasketIcon';
+import { setRestaurant } from '../features/restaurantSlice';
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   
   const {params: {
     id,
@@ -22,6 +24,22 @@ const RestaurantScreen = () => {
     long,
     lat,
   }} = useRoute();
+
+  useEffect(() => {
+    dispatch(setRestaurant({
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      short_description,
+      dishes,
+      long,
+      lat,
+    }))
+  }, [dispatch])
+  
 
   useLayoutEffect(() => {
     navigation.setOptions({
