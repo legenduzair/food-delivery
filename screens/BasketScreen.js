2613769a961e70a221bf81d458a27bfbfd8c168a
Uzair Oomer <uzair.oomer@jdplc.com>
2024-03-15 15:23:51 +0000

    add order preparation screen with animations libraries

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -88,7 +88,7 @@ const BasketScreen = () => {
             <Text className="font-extrabold">£{basketTotal + 3.99}</Text>
           </View>
 
-          <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4">
+          <TouchableOpacity onPress={() => navigation.navigate('PreparingOrderScreen')} className="rounded-lg bg-[#00CCBB] p-4">
             <Text className="text-center text-white text-lg font-bold">Place Order</Text>
           </TouchableOpacity>
         </View>
